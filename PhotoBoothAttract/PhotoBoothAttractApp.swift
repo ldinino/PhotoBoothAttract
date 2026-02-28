c43910cc6c932e7a66afd21713ca8dff519c8af4
Luciano DiNino <luciano@corpo.tel>
2026-02-28 11:25:21 -0700

    Initial Commit

---
+++ PhotoBoothAttract/PhotoBoothAttractApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoothAttractApp.swift
+//  PhotoBoothAttract
+//
+//  Created by Luciano DiNino on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoothAttractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
